32aa56f13ec4ef6a6218a31a9c6888683ff2d6a5
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 22:13:00 +0300

    Create Header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  Navbar(v-bind:userInfo="userInfo")
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'Header',
+  components: {
+    Navbar,
+  },
+  props: ['userInfo'],
+}
+</script>
