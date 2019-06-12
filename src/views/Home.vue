6de51229edaea17e4dcf6e01b677590ee80d813b
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 22:04:34 +0300

    Remove HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div.home
+    img(alt="Vue logo" src="../assets/logo.png")
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+  },
 }
 </script>
