a86d45fee681105141cc2c659a807c4dc51e18e5
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 22:11:37 +0300

    Update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  div
+    h1 This is an example about page
+
+    .alert.alert-primary.alert-dismissible.fade.show(role='alert')
+      | With Bootstrap!
+      button.close(type='button', data-dismiss='alert', aria-label='Close')
+        span(aria-hidden='true') ×
 </template>
+
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
