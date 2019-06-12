981ca53528a1bcb92ef4f19e3def1da319678c6f
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-13 01:18:27 +0300

    Update routes

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/about',
@@ -18,12 +18,18 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
     {
       path: '/profile',
       name: 'profile',
-      component: () => import('./views/Profile.vue')
+      component: () => import('./views/Profile.vue'),
+    },
+    {
+      path: '/user/:id',
+      name: 'user-info',
+      component: () => import('./views/UserInfo.vue'),
+      props: true,
     },
   ]
 })
