be511a22b78b94e5225cf445e473f80f94a78367
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-13 01:19:13 +0300

    Create UserInfo component (WIP)

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  Profile(v-if="userInfo" v-bind:userInfo="userInfo")
+  div(v-else).alert.alert-danger.text-center Wrong user ID
+</template>
+
+<script>
+import Profile from './Profile'
+import router from '../router'
+
+
+export default {
+  name: 'UserInfo',
+  // props: ['allUsers'],
+  components: {
+    Profile,
+  },
+  data () {
+    console.log('data!!')
+    return {
+      userInfo: {},
+    }
+  },
+  mounted () {
+    this.prepareUserInfo()
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.prepareUserInfo()
+    next()
+  },
+  methods: {
+    prepareUserInfo () {
+      console.log('prepareUserInfo')
+      // TODO: request to api to get user info!!
+    }
+  }
+}
+</script>
