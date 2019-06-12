4caa06209d0a3dac34fe40e1f978a95effd9099f
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 23:28:04 +0300

    Add route, fix active link class

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  linkExactActiveClass: 'active',
   routes: [
     {
       path: '/',
@@ -18,6 +19,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('./views/Profile.vue')
+    },
   ]
 })
