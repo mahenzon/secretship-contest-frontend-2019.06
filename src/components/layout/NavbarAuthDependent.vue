ca6478fbae48a0b90d2e661bff1675d0eee74c6f
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 22:09:54 +0300

    Create NavbarAuthDependent component

---
+++ src/components/layout/NavbarAuthDependent.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  ul(v-if="userInfo.authenticated").navbar-nav.ml-auto
+    li.nav-item
+      router-link(to="/profile").nav-link Profile info
+    button(@click="logout()").btn.btn-outline-dark.my-2.my-sm-0 Logout
+  ul(v-else).navbar-nav.ml-auto
+    a(href="https://t.me/myawbot?start=login" target="blank").btn.btn-outline-primary.my-2.my-sm-0 Login
+</template>
+
+<script>
+import router from '../../router'
+
+export default {
+  name: 'NavbarAuthDependent',
+  props: ['userInfo'],
+  data() {
+    return {
+      authenticated: true,
+      // authenticated: false,
+    }
+  },
+  methods: {
+    logout() {
+      this.authenticated = false
+      router.push('/')
+    },
+  },
+}
+</script>
