e3c97aa72c37a61d5ed3ecf6109c4ea422105e53
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-12 22:10:38 +0300

    Create Navbar component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  nav.navbar.navbar-expand-lg.navbar-light.bg-light
+    .navbar-brand Contest
+    button.navbar-toggler(type='button', data-toggle='collapse', data-target='#navbarText', aria-controls='navbarText', aria-expanded='false', aria-label='Toggle navigation')
+      span.navbar-toggler-icon
+    #navbarText.collapse.navbar-collapse
+      ul.navbar-nav.mr-auto
+        li.nav-item
+          router-link(to="/").nav-link Home 
+        li.nav-item
+          router-link(to="/about").nav-link About
+      NavbarAuthDependent(v-bind:userInfo="userInfo")
+</template>
+
+<script>
+import NavbarAuthDependent from './NavbarAuthDependent'
+
+export default {
+  name: 'Navbar',
+  components: {
+    NavbarAuthDependent,
+  },
+  props: ['userInfo'],
+}
+</script>
