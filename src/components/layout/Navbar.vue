5969fb5489115e1158932967ae647fe90e5fb1c0
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-13 00:05:27 +0300

    Update navbar: do not collapse Home link

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
   nav.navbar.navbar-expand-lg.navbar-light.bg-light
     .navbar-brand Contest
+    ul.navbar-nav.mr-auto
+      li.nav-item
+        router-link(to="/").nav-link Home 
     button.navbar-toggler(type='button', data-toggle='collapse', data-target='#navbarText', aria-controls='navbarText', aria-expanded='false', aria-label='Toggle navigation')
       span.navbar-toggler-icon
     #navbarText.collapse.navbar-collapse
       ul.navbar-nav.mr-auto
-        li.nav-item
-          router-link(to="/").nav-link Home 
         li.nav-item
           router-link(to="/about").nav-link About
       NavbarAuthDependent(v-bind:userInfo="userInfo")
