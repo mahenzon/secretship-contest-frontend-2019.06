80168b992ede3313af895e0f43a543dcafdcd5ee
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 15:33:58 +0300

    Create List User component

---
+++ src/views/ListUser.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .clearfix
+    img.rounded-circle.img-fluid.avatar-mini.mr-2(alt="Avatar", v-bind:src="`http://localhost:3001/telegram-media/${user.profile_photo_id}`")
+    div.py-2
+      div {{ user.first_name }} {{ user.last_name }}
+      small Join date: {{ joined (user) }}
+</template>
+
+<script>
+import joinDateString from '../helpers/join-date'
+
+export default {
+  name: 'ListUser',
+  props: ['user'],
+  methods: {
+    joined (user) {
+      return joinDateString({ join_date: user.join_date, language_code: navigator.language || navigator.userLanguage })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .avatar-mini {
+    width: 64px;
+    height: 64px;
+    object-fit: cover;
+    -o-object-fit: cover;
+    float: left;
+  }
+</style>
