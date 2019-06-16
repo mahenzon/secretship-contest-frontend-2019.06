f24160bc2a002266c28a7be01ad290d330af745a
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 20:52:46 +0300

    Add scrollable div for users list

--- src/views/UsersList.vue
+++ src/views/UsersList.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
   div
     h3 Users:
-    div.my-3(v-bind:key="user.user_id" v-for="user in allUsers")
-      ListUser(v-bind:user="user" v-bind:userInfo="userInfo")
+    .users
+      div.my-3(v-bind:key="user.user_id" v-for="user in allUsers")
+        ListUser(v-bind:user="user" v-bind:userInfo="userInfo")
 </template>
 
 <script>
@@ -16,3 +17,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .users {
+    height: 80vh;
+    overflow-y: scroll;
+  }
+</style>
+
