bedceb2e9897a55b9fa8dbede919043a4c14b2b8
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 23:21:52 +0300

    Implement scrollable div with all users + offset

--- src/views/UsersList.vue
+++ src/views/UsersList.vue
@@ -1,25 +1,80 @@
 <template lang="pug">
   div
     h3 Users:
-    .users
+    #users-list
       div.my-3(v-bind:key="user.user_id" v-for="user in allUsers")
         ListUser(v-bind:user="user" v-bind:userInfo="userInfo")
 </template>
 
 <script>
+import axios from 'axios';
 import ListUser from './ListUser'
 
 export default {
   name: 'UsersList',
-  props: ['allUsers', 'userInfo'],
+  props: ['userInfo'],
   components: {
     ListUser,
-  }
+  },
+  data() {
+    return {
+      nowFetching: false,
+      totalUsersAvailable: 100,  // make > than allUsers lenth to allow first fetch
+      allUsers: [],
+      usersIds: new Set(),
+    }
+  },
+  created() {
+    this.loadMoreUsers()
+  },
+  mounted() {
+    const elem = document.getElementById('users-list')
+    elem.addEventListener('scroll', this.handleScroll);
+    console.log('scrolling Injected');
+  },
+  beforeDestroy() {
+    // Donno why JS cannot get this element
+    // const elem = document.getElementById('users-list')
+    // elem.removeEventListener('scroll', this.handleScroll);
+    // console.log('scrolling Destroyed');
+  },
+  methods: {
+    handleScroll: function(e) {
+      const { scrollTop, clientHeight, scrollHeight } = document.getElementById('users-list')
+      if (scrollTop + clientHeight + 42 >= scrollHeight) {
+        console.log('Hit bottom!', scrollTop)
+        this.loadMoreUsers()
+      }
+    },
+    async loadMoreUsers() {
+      if (this.nowFetching || (this.allUsers.length >= this.totalUsersAvailable)) return
+      console.log('load more!!!')
+      this.nowFetching = true
+      try {
+        const url = '/api/users'
+        // const url = 'http://localhost:3001/api/users'
+        const params = { offset: this.allUsers.length, limit: 50 }
+        const response = await axios.get(url, { params })
+        const { users, total } = response.data
+        this.totalUsersAvailable = total
+        this.addUsersSafe(users)
+      } catch (error) {
+        console.log(error)
+      }
+      this.nowFetching = false
+    },
+    addUsersSafe(users) {
+      users.forEach((element) => {
+        if (this.usersIds.has(element.user_id)) return
+        this.allUsers.push(element)
+      })
+    },
+  } 
 }
 </script>
 
 <style scoped>
-  .users {
+  #users-list {
     height: 80vh;
     overflow-y: scroll;
   }
