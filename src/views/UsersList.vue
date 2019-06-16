0d1840d83c014278301a54854b8f6e91a547939c
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 15:34:18 +0300

    Create UsersList component

---
+++ src/views/UsersList.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    h3 Users:
+    div.my-3(v-bind:key="user.user_id" v-for="user in allUsers")
+      ListUser(v-bind:user="user")
+</template>
+
+<script>
+import ListUser from './ListUser'
+
+export default {
+  name: 'UsersList',
+  props: ['allUsers'],
+  components: {
+    ListUser,
+  }
+}
+</script>
