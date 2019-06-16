ffe206b483db353481c0de76cf4a36a7180a26b7
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 13:13:52 +0300

    Create Login component

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  h2 Logging in as {{ this.$route.query.first_name }}...
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Login',
+  props: ['userInfo'],
+  components: {
+  },
+  mounted () {
+    this.$emit('login-user', this.$route.query)
+  }
+}
+</script>
