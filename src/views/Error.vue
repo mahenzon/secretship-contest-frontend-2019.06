bb68db5c97d47c63d29a2876d7e44a9ca70f057f
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 13:14:03 +0300

    Create erro component

---
+++ src/views/Error.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div.alert.alert-danger.text-center {{ this.errorMessage }}
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  props: ['errorMessage'],
+}
+</script>
