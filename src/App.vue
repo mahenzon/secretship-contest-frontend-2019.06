886d97d5737778930ff7590f172faae1aea6e5d1
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 13:36:31 +0300

    Implement login, fetch user on page open

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div#app.container
     Header(v-bind:userInfo="userInfo")
-    router-view(v-bind:userInfo="userInfo" v-bind:allUsers="allUsers")
+    router-view(v-bind:userInfo="userInfo" v-bind:allUsers="allUsers" v-bind:errorMessage="errorMessage" v-on:login-user="loginUser")
     Footer
 </template>
 
@@ -15,8 +15,10 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 
 import user from './user'
-const { createUser } = user
+import router from './router';
 
+const { createUser } = user
+axios.defaults.withCredentials = true
 
 export default {
   name: 'app',
@@ -26,27 +28,40 @@ export default {
   },
   data() {
     return {
-      userInfo: createUser({
-        user_id: 3595399,
-        username: 'surik00',
-        first_name: 'Suren',
-        last_name: 'Khorenyan',
-        language_code: 'en',
-        join_date: 1560291120000,
-        profile_photo_id: 'AgADAgADKNkxG4fcNgAB7D6lxNdM8FRJDUsNAARBpmW0rIZhjuLBAAIC',
-      }),
+      errorMessage: 'Unexpected error!',
+      userInfo: {},
       allUsers: [createUser({ first_name: 'John', user_id: 12345 })],
     }
   },
+  methods: {
+    loginUser(params) {
+      if (Object.keys(params).length === 0) {  // Check if empty object
+        return router.push('/')
+      }
+      axios.post('http://localhost:3001/api/login', params)
+        .then((response) => {
+          this.userInfo = createUser(response.data.user)
+          router.push('/profile')
+        })
+        .catch((error) => {
+          this.errorMessage = error.response.data.error
+          if(this.userInfo.user_id) {
+            this.userInfo.logout()
+          }
+          router.push('/error')
+        })
+    },
+  },
   created() {
-    axios.get('https://httpbin.org/get')
+    axios.get('http://localhost:3001/api/getMe')
       .then((response) => {
-        // handle success
-        console.log(response);
+        this.userInfo = createUser(response.data.user)
       })
       .catch((error) => {
-        // handle error
-        console.log(error);
+        if (error.response.status != 401) { 
+          // Expecting Unauthorized only
+          console.log(error.response)
+        }
       })
   }
 }
