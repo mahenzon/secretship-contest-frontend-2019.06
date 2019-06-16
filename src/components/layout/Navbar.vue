dbd7ba2b09ce08acfe366b682884aef12495e26f
Suren Khorenyan <surenkhorenyan@gmail.com>
2019-06-16 23:39:18 +0300

    Change navbar collision size (width)

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-  nav.navbar.navbar-expand-lg.navbar-light.bg-light.mb-3
+  nav.navbar.navbar-expand-lg.navbar-light.bg-light.mb-3.navbar-expand-sm
     .navbar-brand Contest
     ul.navbar-nav.mr-auto
       li.nav-item
-        router-link(to="/").nav-link Home 
+        router-link(to="/").nav-link Home
     button.navbar-toggler(type='button', data-toggle='collapse', data-target='#navbarText', aria-controls='navbarText', aria-expanded='false', aria-label='Toggle navigation')
       span.navbar-toggler-icon
     #navbarText.collapse.navbar-collapse
